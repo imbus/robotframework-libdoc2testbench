1182603264edab0c6a5d90219ba0cfd47c276812
Maximilian Birkenhagen <maximilian.birkenhagen@imbus.de>
2021-03-03 16:56:14 +0100

    changed method to write zipfile

--- src/Libdoc2TestBench/__init__.py
+++ src/Libdoc2TestBench/__init__.py
@@ -1,13 +1,20 @@
-from shutil import make_archive
-
+from zipfile import ZipFile
 from .testbenchwriter import Libdoc2TestBenchWriter
 from robot.libdocpkg import LibraryDocumentation
+from robot.utils.robotio import file_writer
 
 __version__ = "0.0.1"
 
 def create_project_dump(lib_or_res: str, outfile_path: str):
     libdoc = LibraryDocumentation(lib_or_res)
-    with open(outfile_path, "w") as outfile:
+    libdoc.convert_docs_to_html()
+    with open('project-dump.xml', "w", encoding='UTF-8') as outfile:
         Libdoc2TestBenchWriter().write(libdoc, outfile)
-    make_archive(outfile_path, 'zip')
+    write_zip_file(outfile_path)
+
+    
     print("Successfully written zip file")
+
+def write_zip_file(outfile_path):
+    with ZipFile(outfile_path, 'w') as zip_file:
+        zip_file.write('project-dump.xml', 'project-dump.xml')
