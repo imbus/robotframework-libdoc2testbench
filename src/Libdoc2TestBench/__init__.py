1536b81da57fe9273d109059a986cc96b7132b3e
Maximilian Birkenhagen <maximilian.birkenhagen@imbus.de>
2021-03-04 13:40:25 +0100

    make output optional parameter and check for exisiting xml files

--- src/Libdoc2TestBench/__init__.py
+++ src/Libdoc2TestBench/__init__.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from pathlib import Path
 from zipfile import ZipFile
 from .testbenchwriter import Libdoc2TestBenchWriter
@@ -7,19 +8,24 @@ from robot.utils.robotio import file_writer
 
 __version__ = "0.0.1"
 
+
 def create_project_dump(lib_or_res: str, outfile_path: str):
+    # Check for already existing project-dump.xml
+    if Path('project-dump.xml').is_file():
+        user_input = input(
+            'project-dump.xml already exists... overwrite? y/n? \n')
+        if user_input.lower() not in ['y', 'yes']:
+            print('stopped execution')
+            sys.exit()
+            
     libdoc = LibraryDocumentation(lib_or_res)
     libdoc.convert_docs_to_html()
-    if Path('project-dump.xml').is_file():
-        user_input = input('project-dump.xml already exists... overwrite? y/n? \n')
-        if user_input.lower() in ['y', 'yes']:
-            with open('project-dump.xml', "w", encoding='UTF-8') as outfile:
-                Libdoc2TestBenchWriter().write(libdoc, outfile)
-            write_zip_file(outfile_path)
-            os.remove('project-dump.xml')
-            print("Successfully written zip file")
-        else:
-            print('stopped execution')
+    with open('project-dump.xml', "w", encoding='UTF-8') as outfile:
+        Libdoc2TestBenchWriter().write(libdoc, outfile)
+    write_zip_file(outfile_path)
+    os.remove('project-dump.xml')
+    print("Successfully written zip file")
+
 
 def write_zip_file(outfile_path):
     with ZipFile(outfile_path, 'w') as zip_file:
--- src/Libdoc2TestBench/__main__.py
+++ src/Libdoc2TestBench/__main__.py
@@ -6,14 +6,15 @@ from Libdoc2TestBench import create_project_dump
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="Robot Framework Libdoc Extension that generates imbus TestBench Library import formats.",
-        usage=f"Libdoc2TestBench <LIBRARY> <output.zip>",
+        usage=f"Libdoc2TestBench <LIBRARY> -o <output.zip>",
         prog='Libdoc2TestBench')
-    parser.add_argument("library_or_resource", help="RF library or resource", default='project-dump.zip')
-    parser.add_argument('--outfile_path', '-o',nargs='?', help="path to write output, default=project-dump.zip")
+    parser.add_argument("library_or_resource",
+                        help="RF library or resource", default='project-dump.zip')
+    parser.add_argument('-o', '--outfile_path', nargs='?',
+                        help="path to write output, default=project-dump.zip")
     args = parser.parse_args()
 
     lib = args.library_or_resource
-    output_path = args.outfile_path
-    
-    create_project_dump(lib, outfile_path)
+    outfile_path = args.outfile_path
 
+    create_project_dump(lib, outfile_path)
