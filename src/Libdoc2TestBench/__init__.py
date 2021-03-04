9ca012902ed50cbf4086d5c984ef4106323e9d38
Maximilian Birkenhagen <maximilian.birkenhagen@imbus.de>
2021-03-04 13:08:29 +0100

    added argparser and check for existing xml file

--- src/Libdoc2TestBench/__init__.py
+++ src/Libdoc2TestBench/__init__.py
@@ -1,3 +1,5 @@
+import os
+from pathlib import Path
 from zipfile import ZipFile
 from .testbenchwriter import Libdoc2TestBenchWriter
 from robot.libdocpkg import LibraryDocumentation
@@ -8,10 +10,16 @@ __version__ = "0.0.1"
 def create_project_dump(lib_or_res: str, outfile_path: str):
     libdoc = LibraryDocumentation(lib_or_res)
     libdoc.convert_docs_to_html()
-    with open('project-dump.xml', "w", encoding='UTF-8') as outfile:
-        Libdoc2TestBenchWriter().write(libdoc, outfile)
-    write_zip_file(outfile_path)
-    print("Successfully written zip file")
+    if Path('project-dump.xml').is_file():
+        user_input = input('project-dump.xml already exists... overwrite? y/n? \n')
+        if user_input.lower() in ['y', 'yes']:
+            with open('project-dump.xml', "w", encoding='UTF-8') as outfile:
+                Libdoc2TestBenchWriter().write(libdoc, outfile)
+            write_zip_file(outfile_path)
+            os.remove('project-dump.xml')
+            print("Successfully written zip file")
+        else:
+            print('stopped execution')
 
 def write_zip_file(outfile_path):
     with ZipFile(outfile_path, 'w') as zip_file:
--- src/Libdoc2TestBench/__main__.py
+++ src/Libdoc2TestBench/__main__.py
@@ -1,10 +1,19 @@
 import sys
+import argparse
 from os import path
 from Libdoc2TestBench import create_project_dump
 
 if __name__ == "__main__":
-    print(sys.argv[0])
-    if len(sys.argv) == 3:
-        create_project_dump(sys.argv[1], sys.argv[2])
-    else:
-        create_project_dump("RBGLibrary", 'project-dump.zip')
+    parser = argparse.ArgumentParser(
+        description="Robot Framework Libdoc Extension that generates imbus TestBench Library import formats.",
+        usage=f"Libdoc2TestBench <LIBRARY> <output.zip>",
+        prog='Libdoc2TestBench')
+    parser.add_argument("library_or_resource", help="RF library or resource", default='project-dump.zip')
+    parser.add_argument('--outfile_path', '-o',nargs='?', help="path to write output, default=project-dump.zip")
+    args = parser.parse_args()
+
+    lib = args.library_or_resource
+    output_path = args.outfile_path
+    
+    create_project_dump(lib, outfile_path)
+
