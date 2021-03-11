c92a4b35fd2f88d638fc7ab0b6adb95a26cbcb52
Maximilian Birkenhagen <maximilian.birkenhagen@imbus.de>
2021-03-11 09:55:43 +0100

    restructured __init__ & __main__ with new entry point - added new parameters
    TODO: -s specdocformat must still be implemented

--- src/Libdoc2TestBench/__init__.py
+++ src/Libdoc2TestBench/__init__.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import argparse
 from pathlib import Path
 from zipfile import ZipFile
 
@@ -9,7 +10,44 @@ from robot.libdocpkg import LibraryDocumentation
 __version__ = "0.0.1"
 
 
-def create_project_dump(lib_or_res: str, outfile_path: str):
+def run():
+    """Entry point for the Libdic2TestBench module."""
+    parser = argparse.ArgumentParser(
+        description="Robot Framework Libdoc Extension that generates imbus \
+            TestBench Library import formats. \n The easiest way to run \
+            Libdoc2TestBench is just using the `Libdoc2TestBench` command  \
+            and giving it one resource or library to generate a zip-file at \
+            the current location. However, this module can also be executed \
+            via python -m Libdoc2TestBench <LIBRARY>.",
+        usage=f"Libdoc2TestBench <LIBRARY> <output.zip>",
+        prog='Libdoc2TestBench',
+        epilog='Example: python -m Libdoc2TestBench Browser My-Browser-Dump.zip')
+    parser.add_argument("library_or_resource",
+                        help="RF library or resource")
+    parser.add_argument('outfile_path', nargs='?', default='project-dump.zip',
+                        help="optional path to write output, default = project-dump.zip")
+    parser.add_argument('-s', '--specdocformat', choices=['HTML', 'RAW'],
+                        help="Specifies the documentation format used with XML and JSON spec files. `raw` means preserving the original documentation format and `html` means converting documentation to HTML. The default is `html`.")
+    parser.add_argument('-F', '--docformat', choices=['ROBOT', 'HTML', 'TEXT', 'REST'],
+                        help="Specifies the source documentation format. Possible values are Robot Framework's documentation format, HTML, plain text, and reStructuredText. The default value can be specified in library source code and the initial default value is `ROBOT`.")
+    parser.add_argument(
+        '-n', '--name', help="Sets the name of the documented library or resource.")
+    parser.add_argument(
+        '-v', '--version', help="Sets the version of the documented library or resource written in the description.")
+    args = parser.parse_args()
+
+    lib = args.library_or_resource
+    outfile_path = args.outfile_path
+    specdocformat = args.specdocformat
+    docformat = args.docformat
+    lib_name = args.name
+    lib_version = args.version
+
+    create_project_dump(lib, outfile_path, specdocformat,
+                        docformat, lib_name, lib_version)
+
+
+def create_project_dump(lib_or_res: str, outfile_path: str, specdocformat, docformat, lib_name, lib_version):
     # Check for already existing project-dump.xml
     if Path('project-dump.xml').is_file():
         user_input = input(
@@ -18,8 +56,8 @@ def create_project_dump(lib_or_res: str, outfile_path: str):
             print('stopped execution')
             sys.exit()
 
-    libdoc = LibraryDocumentation(lib_or_res)
-    libdoc.convert_docs_to_html()
+    libdoc = LibraryDocumentation(lib_or_res, lib_name, lib_version, docformat)
+    libdoc.convert_docs_to_html() #TODO: -s param
     with open('project-dump.xml', "w", encoding='UTF-8') as outfile:
         Libdoc2TestBenchWriter().write(libdoc, outfile)
     write_zip_file(outfile_path)
--- src/Libdoc2TestBench/__main__.py
+++ src/Libdoc2TestBench/__main__.py
@@ -1,21 +1,4 @@
-import sys
-import argparse
-
-from Libdoc2TestBench import create_project_dump
+from Libdoc2TestBench import run
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description="Robot Framework Libdoc Extension that generates imbus TestBench Library import formats.",
-        usage=f"Libdoc2TestBench <LIBRARY> -o <output.zip>",
-        prog='Libdoc2TestBench',
-        epilog='Example: python -m Libdoc2TestBench Browser -o My-Browser-Dump.zip')
-    parser.add_argument("library_or_resource",
-                        help="RF library or resource")
-    parser.add_argument('-o', '--outfile_path', nargs='?', default='project-dump.zip',
-                        help="path to write output, default=project-dump.zip")
-    args = parser.parse_args()
-
-    lib = args.library_or_resource
-    outfile_path = args.outfile_path
-
-    create_project_dump(lib, outfile_path)
+    run()
