29d81793bd9510ce40218d5ac3ef6953810b7b30
HenrikSchuette <Henrik.schuette@gmx.de>
2023-12-12 22:43:11 +0100

    fixed datatype count

--- libdoc2testbench/utils.py
+++ libdoc2testbench/utils.py
@@ -10,6 +10,5 @@ def replace_invalid_characters(name: str) -> str:
 def print_stat(libdoc: LibraryDoc, num_interactinos: int, num_datatypes: int) -> None:
     print(f"{libdoc.type.lower()}: {libdoc.name}")
     print(f"  {num_interactinos} Interactions")
-    data_types = libdoc.to_dictionary()["typedocs"]
-    if data_types:
+    if num_datatypes > 0:
         print(f"  {num_datatypes} Data Types")
